c6422aff5289c60dbb2ce14c688b1afa98ecdc3e
AlexElaryan <155388139+AlexElaryan@users.noreply.github.com>
2024-11-02 13:58:33 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,12 +15,24 @@ closePopup.onclick = () => {
 }
 
 
+let phoneInp = document.querySelector('.phone-number');
+let phoneRegex = /^\+?\d*$/;
+
+phoneInp.oninput = (e) => {
+    if (!phoneRegex.test(e.target.value)) {
+        e.target.value = e.target.value.slice(0, -1);
+    }
+};
+
 let confirmSignIn = document.querySelector('.signin-confirm');
 let signinValues = document.querySelectorAll('.signin-item input');
 let agreement = document.getElementById('agreement');
+let email = document.querySelector('.email');
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 
 confirmSignIn.onclick = () => {
-    let allValid = true; 
+    let allValid = true;
 
     signinValues.forEach(inp => {
         if (inp.value.trim() === '') {
@@ -36,4 +48,10 @@ confirmSignIn.onclick = () => {
     } else if (agreement.checked) {
         agreement.parentElement.classList.remove('invalid-feedback-agreement');
     }
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add('invalid-feedback');
+    } else if (email.value.match(emailRegex)) {
+        email.classList.remove('invalid-feedback');
+    };
 };
